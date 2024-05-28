bfd83775b9b87f15f911571e2eac1344065c9a84
Eric-Johnston <eric.mjohnston@protonmail.com>
2024-05-27 20:24:43 -0400

    initial commit

---
+++ src/main/java/com/ericjohnston/Cst323BlogsiteApplication.java
@@ -0,0 +1,14 @@
+package com.ericjohnston;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst323BlogsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst323BlogsiteApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/ericjohnston/Cst323BlogsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericjohnston;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst323BlogsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
