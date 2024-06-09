00f2942ca6370d56feef43091a054268dfe2c5c6
Eric-Johnston <eric.mjohnston@protonmail.com>
2024-06-09 16:59:56 -0400

    debug code added

--- src/main/java/com/ericjohnston/Cst323BlogsiteApplication.java
+++ src/main/java/com/ericjohnston/Cst323BlogsiteApplication.java
@@ -17,6 +17,10 @@ public class Cst323BlogsiteApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Cst323BlogsiteApplication.class, args);
 		System.out.println("Build success! Application running @ localhost:8080 🌍");
+		// Log environment variables for debugging
+        System.out.println("DB URL: " + System.getenv("MYSQLCONNSTR_DB_URL"));
+        System.out.println("DB Username: " + System.getenv("MYSQLCONNSTR_DB_USERNAME"));
+        System.out.println("DB Password: " + System.getenv("MYSQLCONNSTR_DB_PASSWORD"));
 	}
 
 }
